0d01fa9ed0961fd240a5e8454ded14e99782e72c
paraparata <kido.151@gmail.com>
2019-11-28 02:35:52 +0700

    Update dev

---
+++ lib/models/Database.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final dbAplikasi = Firestore.instance;
+String user = "nama";
+// String email = "email@gmail.com";
+String pw = "1234";
+String plat = "D 1234 AS";
+String hp = "085555555";
+
+// void signUp() async {
+//   await dbAplikasi
+//     .collection("user")
+//     .document("user")
+//     .setData({
+//         'user' : [user, pw, plat, hp]
+//       });
+// }
+
+// void getUser() {
+//   dbAplikasi
+//       .collection("user")
+//       .getDocuments()
+//       .then((QuerySnapshot snapshot) {
+//     snapshot.documents.forEach((f) => print('${f.data}}'));
+//   });
+// }
+
+void getSlot() {
+  dbAplikasi
+      .collection("slot")
+      .getDocuments()
+      .then((QuerySnapshot snapshot) {
+    snapshot.documents.forEach((f) => print('${f.data}}'));
+  });
+}
+
+// void updateUser() {
+//   try {
+//     dbAplikasi
+//         .collection('user')
+//         .document(user)
+//         .updateData({'user' : [user, pw, plat, hp]});
+//   } catch (e) {
+//     print(e.toString());
+//   }
+// }
